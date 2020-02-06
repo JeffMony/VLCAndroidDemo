f1e357261495e37312db4e4afa65488e194187e3
tianpeng <tianpeng@tianpengdeMacBook-Pro.local>
2020-02-06 10:50:37 +0800

    提交demo代码

--- app/src/main/java/com/android/vlcdemo/MainActivity.java
+++ app/src/main/java/com/android/vlcdemo/MainActivity.java
@@ -1,19 +1,347 @@
 package com.android.vlcdemo;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
+import android.content.pm.PackageManager;
+import android.content.res.Configuration;
+import android.graphics.PixelFormat;
 import android.os.Bundle;
+import android.os.Handler;
+import android.util.Log;
+import android.view.SurfaceView;
+import android.view.TextureView;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.ViewStub;
+import android.widget.FrameLayout;
+import android.widget.Toast;
 
-import org.videolan.libvlc.media.MediaPlayer;
+import org.videolan.libvlc.LibVLC;
+import org.videolan.libvlc.Media;
+import org.videolan.libvlc.MediaPlayer;
+import org.videolan.libvlc.interfaces.IVLCVout;
 
-public class MainActivity extends AppCompatActivity {
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity implements IVLCVout.OnNewVideoLayoutListener {
+
+    private static final String WRITE_EXTERNAL_STORAGE = "android.permission.WRITE_EXTERNAL_STORAGE";
+    private static final int REQUEST_PERMISSION_OK = 0x1;
+    private static final boolean USE_SURFACE_VIEW = true;
+    private static final boolean ENABLE_SUBTITLES =true;
+    private static final String ASSET_FILENAME = "love.mkv";
+    private static final String TAG = "VLCDemo";
+    private static final int SURFACE_BEST_FIT = 0;
+    private static final int SURFACE_FIT_SCREEN = 1;
+    private static final int SURFACE_FILL = 2;
+    private static final int SURFACE_16_9 = 3;
+    private static final int SURFACE_4_3 = 4;
+    private static final int SURFACE_ORIGINAL = 5;
+    private static int CURRENT_SIZE = SURFACE_BEST_FIT;
+
+    private FrameLayout mVideoSurfaceFrame = null;
+    private SurfaceView mVideoSurface = null;
+    private SurfaceView mSubtitlesSurface = null;
+    private TextureView mVideoTexture = null;
+    private View mVideoView = null;
+    private int mVideoHeight = 0;
+    private int mVideoWidth = 0;
+    private int mVideoVisibleHeight = 0;
+    private int mVideoVisibleWidth = 0;
+    private int mVideoSarNum = 0;
+    private int mVideoSarDen = 0;
+
+    private View.OnLayoutChangeListener mOnLayoutChangeListener = null;
+    private Handler mHandler = new Handler();
+    private LibVLC mLibVLC = null;
+    private MediaPlayer mMediaPlayer = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        checkPermission();
 
-        MediaPlayer player = new MediaPlayer(this);
+        final ArrayList<String> args = new ArrayList<>();
+        args.add("-vvv");
+        mLibVLC = new LibVLC(this, args);
+        mMediaPlayer = new MediaPlayer(mLibVLC);
 
+        mVideoSurfaceFrame = (FrameLayout) findViewById(R.id.video_surface_frame);
+        if (USE_SURFACE_VIEW) {
+            ViewStub stub = (ViewStub) findViewById(R.id.surface_stub);
+            mVideoSurface = (SurfaceView) stub.inflate();
+            if (ENABLE_SUBTITLES) {
+                stub = (ViewStub) findViewById(R.id.subtitles_surface_stub);
+                mSubtitlesSurface = (SurfaceView) stub.inflate();
+                mSubtitlesSurface.setZOrderMediaOverlay(true);
+                mSubtitlesSurface.getHolder().setFormat(PixelFormat.TRANSLUCENT);
+            }
+            mVideoView = mVideoSurface;
+        } else {
+            ViewStub stub = (ViewStub) findViewById(R.id.texture_stub);
+            mVideoTexture = (TextureView) stub.inflate();
+            mVideoView = mVideoTexture;
+        }
+
+    }
+
+    private void checkPermission() {
+        if (ContextCompat.checkSelfPermission(this, WRITE_EXTERNAL_STORAGE)
+                != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, new String[] { WRITE_EXTERNAL_STORAGE }, REQUEST_PERMISSION_OK);
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        if (requestCode == REQUEST_PERMISSION_OK) {
+            if (grantResults.length > 1 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                Toast.makeText(this,  "存储权限已开通", Toast.LENGTH_SHORT).show();
+            } else {
+                Toast.makeText(this,  "存储权限被拒绝", Toast.LENGTH_SHORT).show();
+            }
+        }
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+        final IVLCVout vlcVout = mMediaPlayer.getVLCVout();
+        if (mVideoSurface != null) {
+            vlcVout.setVideoView(mVideoSurface);
+            if (mSubtitlesSurface != null) {
+                vlcVout.setSubtitlesView(mSubtitlesSurface);
+            }
+        } else {
+            vlcVout.setVideoView(mVideoTexture);
+        }
+        vlcVout.attachViews(this);
+
+        try {
+            final Media media = new Media(mLibVLC, getAssets().openFd(ASSET_FILENAME));
+            mMediaPlayer.setMedia(media);
+            media.release();
+        } catch (Exception e) {
+            e.printStackTrace();
+            return;
+        }
+        mMediaPlayer.play();
+
+        if (mOnLayoutChangeListener == null) {
+            mOnLayoutChangeListener = new View.OnLayoutChangeListener() {
+
+                @Override
+                public void onLayoutChange(View v, int left, int top, int right, int bottom, int oldLeft, int oldTop, int oldRight, int oldBottom) {
+                    if (left != oldLeft || top != oldTop || right != oldRight || bottom != oldBottom) {
+                        mHandler.removeCallbacks(mRunnable);
+                        mHandler.post(mRunnable);
+                    }
+                }
+            };
+        }
+        mVideoSurfaceFrame.addOnLayoutChangeListener(mOnLayoutChangeListener);
+    }
+
+    private Runnable mRunnable = new Runnable() {
+        @Override
+        public void run() {
+            updateVideoSurfaces();
+        }
+    };
+
+    private void updateVideoSurfaces() {
+        int sw = getWindow().getDecorView().getWidth();
+        int sh = getWindow().getDecorView().getHeight();
+
+        // sanity check
+        if (sw * sh == 0) {
+            Log.e(TAG, "Invalid surface size");
+            return;
+        }
+
+        mMediaPlayer.getVLCVout().setWindowSize(sw, sh);
+
+        ViewGroup.LayoutParams lp = mVideoView.getLayoutParams();
+        if (mVideoWidth * mVideoHeight == 0) {
+            /* Case of OpenGL vouts: handles the placement of the video using MediaPlayer API */
+            lp.width  = ViewGroup.LayoutParams.MATCH_PARENT;
+            lp.height = ViewGroup.LayoutParams.MATCH_PARENT;
+            mVideoView.setLayoutParams(lp);
+            lp = mVideoSurfaceFrame.getLayoutParams();
+            lp.width  = ViewGroup.LayoutParams.MATCH_PARENT;
+            lp.height = ViewGroup.LayoutParams.MATCH_PARENT;
+            mVideoSurfaceFrame.setLayoutParams(lp);
+            changeMediaPlayerLayout(sw, sh);
+            return;
+        }
+
+        if (lp.width == lp.height && lp.width == ViewGroup.LayoutParams.MATCH_PARENT) {
+            /* We handle the placement of the video using Android View LayoutParams */
+            mMediaPlayer.setAspectRatio(null);
+            mMediaPlayer.setScale(0);
+        }
+
+        double dw = sw, dh = sh;
+        final boolean isPortrait = getResources().getConfiguration().orientation == Configuration.ORIENTATION_PORTRAIT;
+
+        if (sw > sh && isPortrait || sw < sh && !isPortrait) {
+            dw = sh;
+            dh = sw;
+        }
+
+        // compute the aspect ratio
+        double ar, vw;
+        if (mVideoSarDen == mVideoSarNum) {
+            /* No indication about the density, assuming 1:1 */
+            vw = mVideoVisibleWidth;
+            ar = (double)mVideoVisibleWidth / (double)mVideoVisibleHeight;
+        } else {
+            /* Use the specified aspect ratio */
+            vw = mVideoVisibleWidth * (double)mVideoSarNum / mVideoSarDen;
+            ar = vw / mVideoVisibleHeight;
+        }
+
+        // compute the display aspect ratio
+        double dar = dw / dh;
+
+        switch (CURRENT_SIZE) {
+            case SURFACE_BEST_FIT:
+                if (dar < ar)
+                    dh = dw / ar;
+                else
+                    dw = dh * ar;
+                break;
+            case SURFACE_FIT_SCREEN:
+                if (dar >= ar)
+                    dh = dw / ar; /* horizontal */
+                else
+                    dw = dh * ar; /* vertical */
+                break;
+            case SURFACE_FILL:
+                break;
+            case SURFACE_16_9:
+                ar = 16.0 / 9.0;
+                if (dar < ar)
+                    dh = dw / ar;
+                else
+                    dw = dh * ar;
+                break;
+            case SURFACE_4_3:
+                ar = 4.0 / 3.0;
+                if (dar < ar)
+                    dh = dw / ar;
+                else
+                    dw = dh * ar;
+                break;
+            case SURFACE_ORIGINAL:
+                dh = mVideoVisibleHeight;
+                dw = vw;
+                break;
+        }
+
+        // set display size
+        lp.width  = (int) Math.ceil(dw * mVideoWidth / mVideoVisibleWidth);
+        lp.height = (int) Math.ceil(dh * mVideoHeight / mVideoVisibleHeight);
+        mVideoView.setLayoutParams(lp);
+        if (mSubtitlesSurface != null)
+            mSubtitlesSurface.setLayoutParams(lp);
+
+        // set frame size (crop if necessary)
+        lp = mVideoSurfaceFrame.getLayoutParams();
+        lp.width = (int) Math.floor(dw);
+        lp.height = (int) Math.floor(dh);
+        mVideoSurfaceFrame.setLayoutParams(lp);
+
+        mVideoView.invalidate();
+        if (mSubtitlesSurface != null)
+            mSubtitlesSurface.invalidate();
+    }
+
+    private void changeMediaPlayerLayout(int displayW, int displayH) {
+        /* Change the video placement using the MediaPlayer API */
+        switch (CURRENT_SIZE) {
+            case SURFACE_BEST_FIT:
+                mMediaPlayer.setAspectRatio(null);
+                mMediaPlayer.setScale(0);
+                break;
+            case SURFACE_FIT_SCREEN:
+            case SURFACE_FILL: {
+                Media.VideoTrack vtrack = mMediaPlayer.getCurrentVideoTrack();
+                if (vtrack == null)
+                    return;
+                final boolean videoSwapped = vtrack.orientation == Media.VideoTrack.Orientation.LeftBottom
+                        || vtrack.orientation == Media.VideoTrack.Orientation.RightTop;
+                if (CURRENT_SIZE == SURFACE_FIT_SCREEN) {
+                    int videoW = vtrack.width;
+                    int videoH = vtrack.height;
+
+                    if (videoSwapped) {
+                        int swap = videoW;
+                        videoW = videoH;
+                        videoH = swap;
+                    }
+                    if (vtrack.sarNum != vtrack.sarDen)
+                        videoW = videoW * vtrack.sarNum / vtrack.sarDen;
+
+                    float ar = videoW / (float) videoH;
+                    float dar = displayW / (float) displayH;
+
+                    float scale;
+                    if (dar >= ar)
+                        scale = displayW / (float) videoW; /* horizontal */
+                    else
+                        scale = displayH / (float) videoH; /* vertical */
+                    mMediaPlayer.setScale(scale);
+                    mMediaPlayer.setAspectRatio(null);
+                } else {
+                    mMediaPlayer.setScale(0);
+                    mMediaPlayer.setAspectRatio(!videoSwapped ? ""+displayW+":"+displayH
+                            : ""+displayH+":"+displayW);
+                }
+                break;
+            }
+            case SURFACE_16_9:
+                mMediaPlayer.setAspectRatio("16:9");
+                mMediaPlayer.setScale(0);
+                break;
+            case SURFACE_4_3:
+                mMediaPlayer.setAspectRatio("4:3");
+                mMediaPlayer.setScale(0);
+                break;
+            case SURFACE_ORIGINAL:
+                mMediaPlayer.setAspectRatio(null);
+                mMediaPlayer.setScale(1);
+                break;
+        }
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+
+        if (mOnLayoutChangeListener != null) {
+            mVideoSurfaceFrame.removeOnLayoutChangeListener(mOnLayoutChangeListener);
+            mOnLayoutChangeListener = null;
+        }
+
+        mMediaPlayer.stop();
+
+        mMediaPlayer.getVLCVout().detachViews();
+    }
+
+    @Override
+    public void onNewVideoLayout(IVLCVout vlcVout, int width, int height, int visibleWidth, int visibleHeight, int sarNum, int sarDen) {
+        mVideoWidth = width;
+        mVideoHeight = height;
+        mVideoVisibleWidth = visibleWidth;
+        mVideoVisibleHeight = visibleHeight;
+        mVideoSarNum = sarNum;
+        mVideoSarDen = sarDen;
+        updateVideoSurfaces();
     }
 }
